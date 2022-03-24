d695b6a6c225a84f7c098c119d7ca9638c5ca74e
lenintarrillo <lenin.tarrillo.v@gmail.com>
2022-03-24 13:02:59 -0500

    Funciones de aprobación de BUSD y Compra

--- web3ApiMoche.js
+++ web3ApiMoche.js
@@ -17,14 +17,13 @@ async function mintMoche() {
     encodedABI = txInstancia.encodeABI();
     var tx = {
         from: account,
-        to: contractAddress,
+        to: contractMocheAddress,
         data: encodedABI
     };
 
     const rpta = await web3.eth.sendTransaction(tx);
 
     console.log(rpta);
-
 
 }
 
@@ -37,10 +36,52 @@ async function nftMochePriceBase() {
     window.web3 = new Web3(window.ethereum);
     const contract = new web3.eth.Contract(minABI, contractMocheAddress);
     rpta = await contract.methods.nftCurrentPrice().call(); /*input: methodo  */
+    rpta = rpta.substring(0, (rpta.length - 18));
+    console.log(rpta);
 
-    rpta=rpta.substring(0,(rpta.length - 18));
 
-  
+}
+
+
+
+//Aprueba la cantidad de BUSD a retirar de su cuenta
+async function approveBUSD(monto) {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+    window.web3 = new Web3(window.ethereum);
+    const contract = new web3.eth.Contract(abi_BUSD, BUSD_Address);
+    txInstancia = contract.methods.approve(contractMocheAddress, monto + zero18); /*input: methodo  */
+    encodedABI = txInstancia.encodeABI();
+    var tx = {
+        from: account,
+        to: BUSD_Address,
+        data: encodedABI
+    };
+    const rpta = await web3.eth.sendTransaction(tx);
+    console.log(rpta);
+
+}
+
+
+
+
+//Aprueba la cantidad de BUSD a retirar de su cuenta
+async function purchaseNftWithBusd() {
+   
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+    window.web3 = new Web3(window.ethereum);
+    const contract = new web3.eth.Contract(minABI, contractMocheAddress);
+    txInstancia = contract.methods.purchaseNftWithBusd(); /*input: methodo  */
+    encodedABI = txInstancia.encodeABI();
+    var tx = {
+        from: account,
+        to: contractMocheAddress,
+        data: encodedABI
+    };
+
+    const rpta = await web3.eth.sendTransaction(tx);
+
     console.log(rpta);
 
 
